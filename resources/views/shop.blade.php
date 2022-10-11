7c7600be042894043cd6ed61a57972b7aff8f893
VEEX16 <czarnytroll@gmail.com>
2022-10-11 20:01:24 +0200

    strict height

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -106,7 +106,7 @@
 	<!-- start banner Area -->
 	<section class="banner-area">
 		<div class="container">
-			<div class="row fullsrceen align-items-center justify-content-start" style="height:1000px">
+			<div class="row fullsrceen align-items-center justify-content-start">
 				<div class="col-lg-12">
 					<div class="active-banner-slider owl-carousel">
 						<!-- single-slide -->
